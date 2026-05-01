70b1002ece109b5d8ed3a33283edb2956e9e33b0
Oleksii Shapovalov <shapovalex@gmail.com>
2026-04-30 20:53:14 -0400

    Agentic flow, prompt fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+
 import step_1_create_story
 import step_2_get_spec_name
 import step_3_implement_story
@@ -6,14 +8,22 @@ import step_4_code_review
 import step_5_testing
 import step_6_commit_and_push
 
+from dotenv import load_dotenv
+load_dotenv()
+
 async def main():
-    context = {}
-    # await step_1_create_story.execute_step_1()
-    await step_2_get_spec_name.execute_step_2(context)
-    # await step_3_implement_story.execute_step_3(context)
-    # await step_4_code_review.execute_step_4(context)
-    # await step_5_testing.execute_step_5(context)
-    await step_6_commit_and_push.execute_step_6(context)
+    steps = int(os.getenv("AGENTIC_LOOP_STEPS", 1))
+    for i in range(steps):
+        print("----------------------------------")
+        print(f"Performing step {i+1}/{steps}")
+        print("----------------------------------")
+        context = {}
+        await step_1_create_story.execute_step_1()
+        await step_2_get_spec_name.execute_step_2(context)
+        await step_3_implement_story.execute_step_3(context)
+        await step_4_code_review.execute_step_4(context)
+        await step_5_testing.execute_step_5(context)
+        await step_6_commit_and_push.execute_step_6(context)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- step_5_testing.py
+++ step_5_testing.py
@@ -5,7 +5,7 @@ async def execute_step_5(context):
     PROMPT=f"""/bmad-agent-dev
     Current specification is @{context['spec_file']}
     Run the application and verify that everything works as expected. Perform a quick regression testing for APIs and UI.
-    You can use Chrome to test UI.
+    Perform UI testing visually using embedded chrome mcp server.
     Verify UV_PROJECT_ENVIRONMENT variable and set it up to correct value before executing any commands.
     Verify test coverage and linter after fixing issues, fix broken tests and linter even if they were broken by another ticket implementation.
     """
